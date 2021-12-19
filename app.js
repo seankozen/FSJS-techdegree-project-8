5441282ef3d8d3984281146abaae39adabed2538
Sean Kozen <sgkozen@gmail.com>
2021-12-19 16:01:46 +0900

    pug files

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 const sequelize = require('./models').sequelize;
+const pug = require('pug');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
